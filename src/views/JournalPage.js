cefd6198eece3ccafa52bb1188f964627f6f64ce
jiayong <jiayong12323@gmail.com>
2024-06-05 21:00:18 +0800

    able to delete journal from journal main page

--- src/views/JournalPage.js
+++ src/views/JournalPage.js
@@ -11,7 +11,7 @@ export default function JournalPage() {
 
     async function getAllJournal(){ // add this function to useeffect later, also change the params id to take the id of the user that logged in
         const response = await axios.get(API + "/journal", {
-            params : {id: 1} // this is userid
+            params : {id: 2} // this is userid
         })
         const data = response.data;
         setJournalList(data);
@@ -26,23 +26,39 @@ export default function JournalPage() {
         })
     }
 
-    async function deleteJournal(){
+    async function deleteJournal(id){
         await axios.delete(API + "/journal/delete", {
-            params : {id: 1, date: "2024-06-01"} // change the id and date to not hard coded later
+            params : {id: id} // change the id and date to not hard coded later
         })
+        getAllJournal();
     }
 
     useEffect(() => {
         getAllJournal();
     }, []);
 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-CA'); // en-CA format is YYYY-MM-DD
+    };
+
     return (
     <Container>
         <h1 className="my-3">My Journal</h1>
         <Container>
         {     journalList.map((journal) => {
-                return <a href={`/journal/canvas/${journal.journal_id}`}> <div> <li>{journal.journal_id} - {journal.calendar_date}</li> </div> </a>
-        })}
+                return (
+                
+                    <div> 
+                        <a href={`/journal/canvas/${journal.journal_id}`}> <li>Journal Number: {journal.journal_id} - {formatDate(journal.calendar_date)}</li></a>
+                        <Button variant="primary" onClick={async (e) => {
+                    deleteJournal(journal.journal_id);
+                }}>
+                Delete
+                </Button>
+                    </div> 
+                
+        )})}
 
         </Container>
     </Container>
